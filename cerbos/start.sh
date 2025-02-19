6b6d538cc84da90dfe580cd40f4a71ad072a6d34
Aldi Doanta Kurnia <aldidkurnia@gmail.com>
2025-02-19 17:47:01 +0700

    docker: auto-remove container after stop

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-docker run -i -t -p 3592:3592 -p 3593:3593 \
+docker run --rm -i -t -p 3592:3592 -p 3593:3593 \
 	-v "${SCRIPT_DIR}/policies:/policies" \
 	ghcr.io/cerbos/cerbos:latest \
 	server
